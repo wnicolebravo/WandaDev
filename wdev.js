a4b13261cf7d59fd0628138945e003def319d09a
Wanda Nicole Bravo <86751741+wnicolebravo@users.noreply.github.com>
2022-06-30 12:18:38 -0300

    Add files via upload

---
+++ wdev.js
@@ -0,0 +1,9 @@
+
+    const typed2 = new Typed('.Typed', {
+        strings: ['Hola extraño, soy Wanda  y este es mi portafolio.<br> Aquí conocerás un poco más de mí, de mis trabajos y conocimientos.'],
+        typeSpeed: 60,
+        backSpeed: 35,
+        fadeOut: true,
+        loop: true,
+      })
+    ;
